eed3bf3b31f6ae71d6e9cdb96cb1a844ef14d01d
pascal <pascal.zimmerli@protonmail.ch>
2020-11-28 17:24:46 +0100

    Recommendations and hospitals added

--- src/main/java/ch/fhnw/digibp/ProcessApplication.java
+++ src/main/java/ch/fhnw/digibp/ProcessApplication.java
@@ -27,21 +27,33 @@ public class ProcessApplication {
 
     @PostConstruct
     public void postConstruct() {
-        createEntry(AnalysisType.Sars_Cov_2, 20, "Some Recommendation");
-        createEntry(AnalysisType.Sars_Cov_2, 20, "Some Recommendation");
-        createEntry(AnalysisType.Sars_Cov_2, 20, "Some Recommendation");
-        createEntry(AnalysisType.Sars_Cov_2, 20, "Some Recommendation");
-        createEntry(AnalysisType.Sars_Cov_2, 20, "Some Recommendation");
-        createEntry(AnalysisType.Sars_Cov_2, 20, "Some Recommendation");
-        createEntry(AnalysisType.Sars_Cov_2, 20, "Some Recommendation");
-        createEntry(AnalysisType.Sars_Cov_2, 20, "Some Rare Recommendation");
-        createEntry(AnalysisType.Sars_Cov_2, 20, "Some Rare Recommendation");
-        createEntry(AnalysisType.Sars_Cov_2, 50, "Some Other Recommendation");
-        createEntry(AnalysisType.Sars_Cov_2, 50, "Some Other Recommendation");
-        createEntry(AnalysisType.Sars_Cov_2, 50, "Some Other Recommendation");
-        createClient("Uni Spital Basel", "javier_pose88@hotmail.com");  
+        createEntry(AnalysisType.Sars_Cov_2, 0, "No quarantine required.");
+        createEntry(AnalysisType.Sars_Cov_2, 5, "Low virus level, quarantine for 3 days.");
+        createEntry(AnalysisType.Sars_Cov_2, 10, "Low virus level, quarantine for 5 days.");
+        createEntry(AnalysisType.Sars_Cov_2, 20, "Medium virus level, quarantine for 10 days, person(s) with close contact in quarantine for 2 days.");
+        createEntry(AnalysisType.Sars_Cov_2, 50, "High virus level, quarantine for 10 days, person(s) with close contact in quarantine for 5 days.");
+        createEntry(AnalysisType.Sars_Cov_2, 80, "Very high virus level, quarantine for 10 days, person(s) with close contact in quarantine for 10 days.");
+        createEntry(AnalysisType.Ferritin, 10, "Ferritin level too low, diet change highly recommended.");
+        createEntry(AnalysisType.Ferritin, 20, "Ferritin level at the lower end. diet change recommended.");
+        createEntry(AnalysisType.Ferritin, 50, "Average ferritin level. No action required.");
+        createEntry(AnalysisType.Ferritin, 100, "Average ferritin level. No action required.");
+        createEntry(AnalysisType.Ferritin, 600, "Ferritin level above average. Try to eat less meat and other ferritin containing food.");
+        createEntry(AnalysisType.Ferritin, 1000, "Ferritin level too high. Please contact your physician.");
+        createEntry(AnalysisType.Vitamin_D, 10, "Vitamine D level way too low. Get in touch with your phycisian.");
+        createEntry(AnalysisType.Vitamin_D, 20, "Vitamine D level at the lower limit. Consider the intake of food additives.");
+        createEntry(AnalysisType.Vitamin_D, 40, "Vitamien D level in a ideal range. No action required.");
+        createEntry(AnalysisType.Vitamin_D, 60, "Vitamien D level in a ideal range. No action required.");
+        createEntry(AnalysisType.Vitamin_D, 100, "Vitamine D level too high. Reduce food additives if applicable. Otherwise please contact your phycision too discuss your diet.");
+        createEntry(AnalysisType.HIV, 0, "HIV test negative. No action required.");
+        createEntry(AnalysisType.HIV, 1, "HIV test positive. Get in touch with an expert immediately.");
+        createEntry(AnalysisType.Cancer, 0, "Test negative. No cancer cells found. No action required.");
+        createEntry(AnalysisType.Cancer, 1, "Test positive. Get in touch with an expert immediately.");
         createClient("KKH Lörrach", "timo.schoepflin@gmail.com");
-        
+        createClient("Uni Spital Basel", "javier_pose88@hotmail.com");
+        createClient("Kantonsspital Aarau", "philippe.schwank@students.fhnw.ch");
+        createClient("Kantonsspital Basel-Land", "kevin.schaer@students.fhnw.ch");
+        createClient("Kantonsspital Solothurn", "pascal.zimmerli@students.fhnw.ch");
+        createClient("Spital FHNW Muttenz", "andreas.martin@fhnw.ch");
     }
 
     private void createEntry(AnalysisType analysisType, double value, String recommendation) {

1c76606a208d6928168396d37eecb99dce6c9165
Timo Schoepflin <timo.schoepflin@sympany.ch>
2020-12-08 08:22:34 +0100

    Add analysis unit

--- src/main/java/ch/fhnw/digibp/domain/AnalysisType.java
+++ src/main/java/ch/fhnw/digibp/domain/AnalysisType.java
@@ -3,12 +3,22 @@ package ch.fhnw.digibp.domain;
 import java.util.Map;
 
 public enum AnalysisType {
-    Sars_Cov_2, Ferritin, Vitamin_D, HIV, THC, Cancer;
+    Sars_Cov_2("copies (RNA) per mL"), Ferritin("ng/mL"), Vitamin_D("ng/mL"), HIV("copies (RNA) per mL"), THC("ng/mL"), Cancer("");
+
+    private final String unit;
+
+    AnalysisType(String unit) {
+        this.unit = unit;
+    }
 
     public static AnalysisType valueOf(Map<String, Object> map) {
         if (map.containsKey("analysis") && map.get("analysis") != null) {
             return valueOf((String) map.get("analysis"));
         }
         return null;
+    }
+
+    public String getUnit() {
+        return unit;
     }
 }
